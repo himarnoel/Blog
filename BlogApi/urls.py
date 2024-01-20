d3351ab30bbba0bebe7f939b383dd22a40b19069
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2024-01-20 11:31:20 +0100

    using dummy data

--- BlogApi/urls.py
+++ BlogApi/urls.py
@@ -3,8 +3,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-path("homepage/", views.homepage, name="posts_home")   ,
-
+    path("homepage/", views.homepage, name="posts_home"),
+    path("", views.list_Posts, name="list_Post")
 ]
 
 
--- BlogApi/views.py
+++ BlogApi/views.py
@@ -5,12 +5,38 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 
 
+posts= [
+    {
+        'id': 1,
+        'title': 'First Entry',
+        'contents': 'This is the contents of the first entry.'
+    },
+    {
+        'id': 2,
+        'title': 'Second Entry',
+        'contents': 'This is the contents of the second entry.'
+    },
+    {
+        'id': 3,
+        'title': 'Third Entry',
+        'contents': 'This is the contents of the third entry.'
+    },
+    # Add more entries as needed
+]
+
 # Create your views here.
 @api_view(http_method_names=['GET', 'POST'])
 def homepage(request:Request):
     if(request.method=='POST'):
         data=request.data
-        response={"message":"Hello World"}
+        response={"message":"Hello World", "data":data}
         return Response(data=response, status=status.HTTP_201_CREATED)
 
     return Response(data={"message":"Welcome"}, status=status.HTTP_200_OK)
+
+
+
+
+@api_view(http_method_names=['GET'])
+def list_Posts(request:Request):
+    Response(data=posts, status=status.HTTP_200_OK)
