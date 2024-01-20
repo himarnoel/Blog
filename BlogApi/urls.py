b450101a6c87eb91ae7f3edb8d447462bfe207ac
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2024-01-20 11:12:32 +0100

    working on it

---
+++ BlogApi/urls.py
@@ -0,0 +1,10 @@
+from . import views
+
+from django.urls import path
+
+urlpatterns = [
+path("homepage/", views.homepage, name="posts_home")   ,
+
+]
+
+
--- BlogApi/views.py
+++ BlogApi/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
 
 # Create your views here.
+@api_view(http_method_names=['GET', 'POST'])
+def homepage(request:Request):
+    if(request.method=='GET'):
+
+
+    return Response(data={"message":"Welcome"})
