4d323b4bdabadd5f26cec00217d820c1d3d74316
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2024-01-22 00:42:53 +0100

    started working on the account

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,9 @@ from django.contrib.auth.base_user import BaseUserManager
 
 # Create your models here.
 
+class CustomerManager(BaseUserManager):
+    def create_user(self,email, password, **extra_fields ):
+        pass
+    def create_superuser(self,email, password, **extra_fields):
+        pass
+
