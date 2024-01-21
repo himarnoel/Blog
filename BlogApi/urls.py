7e3698c0351fd68f9ee284e6fe6ac2564e667750
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2024-01-21 23:55:05 +0100

    done with classes

--- BlogApi/urls.py
+++ BlogApi/urls.py
@@ -5,9 +5,8 @@ from django.urls import path
 urlpatterns = [
     path("homepage/", views.homepage, name="posts_home"),
     path("", views.PostlistCreateView.as_view(), name="list_Post"),
-    path("<int:post_id>/", views.PostRetrieveUpdateDeleteView.as_view(), name="post_detail"),
-    path("update/<int:post_id>/", views.PostRetrieveUpdateDeleteView.as_view(), name="update_post"),
-     path("delete/<int:post_id>/",views.PostRetrieveUpdateDeleteView.as_view(), name="delete_post")
+    path("<int:post_id>/", views.PostRetrieveUpdateDeleteView.as_view(), name="post_retreiveupdatedelete"),
+  
 ]
 
 
