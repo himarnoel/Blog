fc8e31d96db89eaee53578f838f6d0be28373947
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2024-01-22 12:30:47 +0100

    WORKED ON SIGNUP

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .serializers import SignupSerializer
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.request import Request
 
 # Create your views here.
+class SignupView(generics.GenericAPIView):
+    serializer_class=SignupSerializer
+
+    def post(self, request:Request):
+        data=request.data
+        serializer=self.serializer_class(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            response={
+                "message":"User created successfully"
+            }
+            return Response(data=response, status=status.HTTP_200_OK)
+        return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
