2260179ac0d35780ad11dd4a6d6a4b1a5a09e03b
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2024-01-22 15:18:55 +0100

    ran migrations

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("signup/", views.SignupView.as_view(), name="signup" )
+    path("signup/", views.SignupView.as_view(), name="signup" ),
+    path("login/", views.LoginView.as_view(), name="login")
 ]
